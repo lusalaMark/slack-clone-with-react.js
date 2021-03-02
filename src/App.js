646748c1a2b27433bc192d2bb2319f450e9fa1a6
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-02 15:41:26 +0300

    removing everything that i dont need in the app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello There</h1>
-      </header>
+      <h1>Hello There</h1>
     </div>
   );
 }
