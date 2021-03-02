e10ecc64e26bbefbb1060e7704113df019b9e865
Mark Lusala Mudoga <lusalamark63@gmail.com>
2021-03-02 16:04:27 +0300

    Adding header components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 
 function App() {
   return (
+    // BEM naming convention
     <div className="App">
       <h1>Hello There</h1>
+
+      {/*Header*/}
+
+      {/*Sidebar*/}
+      {/*React-Roter -> Chat Screen*/}
     </div>
   );
 }
